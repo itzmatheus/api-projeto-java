17cb0d4239d9cda12c01487a91dfd0e21d98b77f
Matheus Leite <matheusjoselfm@gmail.com>
2019-06-08 22:33:17 -0300

    Adicionando validacao de cpf e cnpj

--- src/main/java/com/matheusjlfm/apiJava/dto/ClienteNewDTO.java
+++ src/main/java/com/matheusjlfm/apiJava/dto/ClienteNewDTO.java
@@ -1,22 +1,41 @@
 package com.matheusjlfm.apiJava.dto;
 
+import com.matheusjlfm.apiJava.services.validation.ClienteInsert;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
 import java.io.Serializable;
 
+@ClienteInsert
 public class ClienteNewDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
-	private String nome;
-	private String email;
-	private String cpfOuCnpj;
+
+    @NotEmpty(message="Preenchimento obrigatório!")
+    @Length(min=5, max=256, message="O tamanho deve ser entre 5 e 256 caracteres")
+    private String nome;
+
+    @NotEmpty(message="Preenchimento obrigatório!")
+    @Email(message="Email inválido")
+    private String email;
+
+    @NotEmpty(message="Preenchimento obrigatório!")
+    private String cpfOuCnpj;
 	private Integer tipo;
-	
-	private String logradouro;
-	private String numero;
+
+    @NotEmpty(message="Preenchimento obrigatório!")
+    private String logradouro;
+
+    @NotEmpty(message="Preenchimento obrigatório!")
+    private String numero;
 	private String complemento;
 	private String bairro;
-	private String cep;
-	
-	private String telefone1;
+
+    @NotEmpty(message="Preenchimento obrigatório!")
+    private String cep;
+
+    @NotEmpty(message="Preenchimento obrigatório!")
+    private String telefone1;
 	private String telefone2;
 	private String telefone3;
 	
---
+++ src/main/java/com/matheusjlfm/apiJava/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,42 @@
+package com.matheusjlfm.apiJava.services.validation;
+
+import com.matheusjlfm.apiJava.domain.enums.TipoCliente;
+import com.matheusjlfm.apiJava.dto.ClienteNewDTO;
+import com.matheusjlfm.apiJava.resources.exceptions.FieldMessage;
+import com.matheusjlfm.apiJava.services.validation.utils.BR;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+    @Override
+    public void initialize(ClienteInsert ann) {
+    }
+
+    @Override
+    public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+        List<FieldMessage> list = new ArrayList<>();
+
+        try{
+            if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCode()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+                list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
+            }
+
+            if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCode()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+                list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+            }
+
+        }catch (NullPointerException e){
+            list.add(new FieldMessage("tipo", "Campo tipo obrigatório!"));
+        }
+
+        for (FieldMessage e : list) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(e.getMessage())
+                    .addPropertyNode(e.getFieldName()).addConstraintViolation();
+        }
+        return list.isEmpty();
+    }
+}
